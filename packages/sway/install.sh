5074ea72718a6e660b6f3280d162fe4e70cb3d07
Karl Fleischmann <fleischmann.karl@gmail.com>
2020-01-14 22:41:37 +0100

    [sway] Add sway-related configurations
    - Add config for `sway` the desktop environment,
    - add config for `swaylock` the locking mechanism for `sway`,
    - add config for `mako` the notification daemon,
    - add config for `waybar` the status bar, and
    - add installation script to put the configuration files to the
      appropriate places.

---
+++ packages/sway/install.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+CONFIG_DIR="${XDG_CONFIG_HOME:-$HOME/.config}"
+
+echo "# Installing :: sway configuration"
+
+# symlink config file to home config dir
+echo "## Symlinking sway config files to ${CONFIG_DIR}"
+SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
+
+mkdir -p "${CONFIG_DIR}/mako"
+ln -vsf "${SCRIPT_DIR}/mako/config" "${CONFIG_DIR}/mako/"
+
+mkdir -p "${CONFIG_DIR}/sway"
+ln -vsf "${SCRIPT_DIR}/sway/config" "${CONFIG_DIR}/sway/"
+
+mkdir -p "${CONFIG_DIR}/swaylock"
+ln -vsf "${SCRIPT_DIR}/swaylock/config" "${CONFIG_DIR}/swaylock/"
+
+mkdir -p "${CONFIG_DIR}/waybar/scripts"
+ln -vsf "${SCRIPT_DIR}/waybar/config" "${CONFIG_DIR}/waybar/"
+ln -vsf "${SCRIPT_DIR}/waybar/style.css" "${CONFIG_DIR}/waybar/"
+
+echo -e "\n# Note:"
+echo    "## Make sure to create an \`output.conf\` file to setup the background of sway."
+echo -e "## Make sure you install all custom scripts to waybar.\n"
+
+echo "# Installing :: sway configuration - completed"
